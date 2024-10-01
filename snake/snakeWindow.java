9b5a0015f576e31b4d58847c2026c5fc31c4e433
Evan Horn <ehorn01@yahoo.com>
2024-10-01 07:04:24 -0600

    test

--- snake/snakeWindow.java
+++ snake/snakeWindow.java
@@ -1,6 +1,7 @@
 package snake;
 
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class snakeWindow {
         
@@ -11,16 +12,26 @@ public class snakeWindow {
         initialize();
     }
 
+    public static Dimension getScreenDimensions(){
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        return (screenSize);
+    }
+
     public void initialize(){
         frame = new JFrame();
         this.frame.setTitle("Snake Game");
         this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.frame.setSize(1935/2, 1050/2);
+
+        Dimension screenDimensions = getScreenDimensions();
+        int width = (int)screenDimensions.getWidth();
+        int height = (int)screenDimensions.getHeight();
+
+        this.frame.setSize(width, height);
         this.frame.setLocationRelativeTo(null);
         this.frame.setResizable(true);
         this.frame.setVisible(true);
 
-        panel = new snakeGame(null);
+        panel = new snakeGame(screenDimensions);
         this.frame.add(panel);
     }
 }
